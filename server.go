c0fe64e7c9c98d8b2cf6c33ec2855d30e6e541ba
k0963584 <natthapong.ch@kbtg.tech>
2020-09-26 11:31:20 +0700

    encapsulate logger

--- server.go
+++ server.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"database/sql"
 	"encoding/json"
 	"fmt"
@@ -13,7 +12,7 @@ import (
 
 	_ "github.com/lib/pq"
 
-	"go.uber.org/zap"
+	"github.com/Natthapong/hometic/logger"
 )
 
 func main() {
@@ -25,7 +24,7 @@ func main() {
 	}
 
 	r := mux.NewRouter()
-	r.Use()
+	r.Use(logger.Middleware)
 
 	r.Handle("/pair-device", PairDeviceHandler(NewCreatePairDevice(db))).Methods(http.MethodPost)
 
@@ -41,16 +40,6 @@ func main() {
 	log.Fatal(server.ListenAndServe())
 }
 
-func LoggerMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		l := zap.NewExample()
-		l = l.With(zap.Namespace("hometic"), zap.String("I'm", "gopher"))
-
-		c := context.WithValue(r.Context(), "logger", l)
-		next.ServeHTTP(w, r.WithContext(c))
-	})
-}
-
 type Pair struct {
 	DeviceID int64
 	UserID   int64
@@ -58,11 +47,8 @@ type Pair struct {
 
 func PairDeviceHandler(device Device) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		loggerCtx := r.Context().Value("logger")
-		log, ok := loggerCtx.(*zap.Logger)
-		if ok {
-			log.Info("pair-device")
-		}
+		logger.L(r.Context()).Info("pair-device")
+		
 		var p Pair
 		err := json.NewDecoder(r.Body).Decode(&p)
 		if err != nil {
